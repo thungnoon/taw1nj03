aa9c2371a7ad94ca0753c3c7095b16e55f2e462a
thungnoon <kongzheng.zhou@gmail.com>
2024-09-08 16:28:08 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -6,3 +6,4 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 #更改design主题为白色
 # sed -i 's/dark/light/g' feeds/luci/applications/luci-app-design-config/root/etc/config/design
 
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
