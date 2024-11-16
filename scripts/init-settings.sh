2f527b1a37bb6eaa8a68e5cab850677da1215d88
thungnoon <kongzheng.zhou@gmail.com>
2024-11-16 17:20:08 +0800

    Add files via upload

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-#更改默认地址为192.168.5.1
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#更改默认地址为192.168.8.1
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/luci2/bin/config_generate
 
 #更改design主题为白色
 # sed -i 's/dark/light/g' feeds/luci/applications/luci-app-design-config/root/etc/config/design
 
-#theme
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
-rm -rf ../../customfeeds/luci/themes/luci-theme-argon
-rm -rf ../../customfeeds/luci/themes/luci--argon-mod
-rm -rf ./luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+echo "init-settings executed successfully!"
